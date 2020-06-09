2a1939d425e6e76a4f1fda96b9f44b3cde041d0d
Grubneac <grubneac@yandex.ru>
2020-06-09 22:20:03 +0300

    Angular simple table

---
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  //create an array of objects
+  salesPersonList: SalesPerson[] =[
+    new SalesPerson("Anup", "Kumar", "anup.kumar@lu2code.com", 50000),
+    new SalesPerson("Vasea", "Pecus", "vasea.pecus@lu2code.com", 3500),
+    new SalesPerson("Alex", "Grubneac", "grubneac@lu2code.com", 5000000),
+    new SalesPerson("Tanea", "Grubneac", "tgrubneac@lu2code.com", 30000),
+    new SalesPerson("Anton", "Grubneac", "agrubneac@lu2code.com", 10000),
+    new SalesPerson("Polina", "Grubneac", "pgrubneac@lu2code.com", 10700)
+  ];
+ 
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
