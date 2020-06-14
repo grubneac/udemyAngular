82e677cfc119fc09221109b9190adc573b9647cf
Grubneac <grubneac@yandex.ru>
2020-06-14 11:00:27 +0300

    add front end ecommerce

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    sku: string;
+    name: string;
+    description: string;
+    unit: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitInStock: string;
+    dateCreated: Date;
+    lastUpdated:Date;
+    
+}
