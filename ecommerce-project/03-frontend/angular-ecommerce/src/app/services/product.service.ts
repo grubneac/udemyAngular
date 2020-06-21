23cb80603c50ebfa80c32e8cc60dabe59328755f
Grubneac <grubneac@yandex.ru>
2020-06-21 08:55:51 +0300

    create dynamic sidebar

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,13 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product'
 import { map } from 'rxjs/operators'
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products';
+   private baseUrl = 'http://localhost:8080/api/products';
+
+   private categoryURL ='http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,14 +21,28 @@ export class ProductService {
     // need to build URL based on category id 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryURL).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+
 }
 
-interface GetResponse{
+interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory{
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
