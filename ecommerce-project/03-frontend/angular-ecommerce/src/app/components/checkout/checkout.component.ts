aaf059ad6fa2cc352da603dd8596ebe4b898dcb2
Grubneac <grubneac@yandex.ru>
2020-10-25 08:15:12 +0200

    frontend simple validations

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormGroupName } from '@angular/forms';
+import { FormGroup, FormBuilder, FormGroupName, FormControl, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
@@ -31,9 +31,9 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
      this.checkoutFormGroup = this.formBuilder.group({
       customer:  this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        email: new FormControl('',[Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -100,6 +100,10 @@ export class CheckoutComponent implements OnInit {
     */
     
   }
+
+  get firstName() {return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() {return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() {return this.checkoutFormGroup.get('customer.email'); }
   
   copyShippingAdressToBuillingAdress(event){
     if (event.target.checked) {
@@ -116,6 +120,10 @@ export class CheckoutComponent implements OnInit {
 
   onSubmit() {
     console.log("Handling for submit button");
+
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
 
